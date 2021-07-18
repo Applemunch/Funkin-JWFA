e8738dce2687a251556b0134b654fa9b01a85a91
AerialB <applemunch123@outlook.com>
2021-07-18 02:11:07 -0400

    The Funkin Bang
    as in, I just copied the project to github lol

---
+++ ! Build to Windows 32bit.bat
@@ -0,0 +1,6 @@
+@echo off
+color 0a
+@echo on
+echo Building 32bit to Windows...
+lime build windows -release -32
+pause
---
+++ ! Build to Windows.bat
@@ -0,0 +1,6 @@
+@echo off
+color 0a
+@echo on
+echo Building 64bit to Windows...
+lime build windows -release
+pause
